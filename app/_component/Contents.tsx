1889f7b4228e1d73c75777852b280d9d5b1dd278
jeeweb <52396673+jeeweb@users.noreply.github.com>
2024-07-21 21:47:53 +0900

    feat: add scrollTo event

--- app/_component/Contents.tsx
+++ app/_component/Contents.tsx
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import styles from "../../styles/contents.module.css";
 import Courses from "./Courses";
 import Introduction from "./Introduction";
@@ -7,22 +8,88 @@ import RefundPolicy from "./RefundPolicy";
 import Reviews from "./Reviews";
 
 export default function Contents() {
+  const scrollRef = useRef<any>([]);
+  const scrollToArticle = (event: any) => {
+    const value = event.target.value;
+    let position;
+    switch (value) {
+      case "indicator":
+        position = scrollRef.current[0].getBoundingClientRect().top - 140;
+        window.scrollTo({ top: position, behavior: "smooth" });
+        break;
+      case "lecturer":
+        position = scrollRef.current[1].getBoundingClientRect().top - 140;
+        window.scrollTo({ top: position, behavior: "smooth" });
+        break;
+      case "courses":
+        position = scrollRef.current[2].getBoundingClientRect().top - 140;
+        window.scrollTo({ top: position, behavior: "smooth" });
+        break;
+      case "otherContents":
+        position = scrollRef.current[3].getBoundingClientRect().top - 140;
+        window.scrollTo({ top: position, behavior: "smooth" });
+        break;
+      case "reviews":
+        position = scrollRef.current[4].getBoundingClientRect().top - 140;
+        window.scrollTo({ top: position, behavior: "smooth" });
+        break;
+    }
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.indicatorGroup}>
-        <button className={`${styles.indicatorItem} ${styles.itemOn}`}>
+        <button
+          className={`${styles.indicatorItem} ${styles.itemOn}`}
+          value="introduction"
+          onClick={scrollToArticle}
+        >
           소개
         </button>
-        <button className={styles.indicatorItem}>강사</button>
-        <button className={styles.indicatorItem}>목차</button>
-        <button className={styles.indicatorItem}>연관콘텐츠</button>
-        <button className={styles.indicatorItem}>리뷰</button>
+        <button
+          className={styles.indicatorItem}
+          value="lecturer"
+          onClick={scrollToArticle}
+        >
+          강사
+        </button>
+        <button
+          className={styles.indicatorItem}
+          value="courses"
+          onClick={scrollToArticle}
+        >
+          목차
+        </button>
+        <button
+          className={styles.indicatorItem}
+          value="otherContents"
+          onClick={scrollToArticle}
+        >
+          연관콘텐츠
+        </button>
+        <button
+          className={styles.indicatorItem}
+          value="reviews"
+          onClick={scrollToArticle}
+        >
+          리뷰
+        </button>
       </div>
-      <Introduction />
-      <Lecturer />
-      <Courses />
-      <OtherContents />
-      <Reviews />
+      <div ref={(el: any) => (scrollRef.current[0] = el)}>
+        <Introduction />
+      </div>
+      <div ref={(el: any) => (scrollRef.current[1] = el)}>
+        <Lecturer />
+      </div>
+      <div ref={(el: any) => (scrollRef.current[2] = el)}>
+        <Courses />
+      </div>
+      <div ref={(el: any) => (scrollRef.current[3] = el)}>
+        <OtherContents />
+      </div>
+      <div ref={(el: any) => (scrollRef.current[4] = el)}>
+        <Reviews />
+      </div>
       <RefundPolicy />
     </div>
   );
