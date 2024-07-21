6f304494ddada2975a3a8b8be5ddddb1d70174d5
jeeweb <52396673+jeeweb@users.noreply.github.com>
2024-07-21 14:24:07 +0900

    design: contents UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
 import styles from "../styles/detail.module.css";
+import AsideInfo from "./_component/AsideInfo";
+import Banner from "./_component/Banner";
+import Contents from "./_component/Contents";
+
 export default function Home() {
   return (
     <>
-      {/* Banner */}
-      {/* Tab */}
-      {/* Info */}
+      <Banner />
+      <div className={styles.container}>
+        <Contents />
+        <AsideInfo />
+      </div>
     </>
   );
 }
